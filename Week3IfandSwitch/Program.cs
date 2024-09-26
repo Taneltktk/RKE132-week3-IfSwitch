64c49740c3eebe0257609ddf369a137b7e92329b
TanelP <tanel.pool@tktk.ee>
2024-09-26 19:57:57 +0300

    add week3 code

---
+++ Week3IfandSwitch/Program.cs
@@ -0,0 +1,11 @@
+﻿// modulus - %
+
+int a = 25;
+int b = 8;
+
+int c = a % b; //+/-/* - /  
+//jagamistehte vastuseks saame 3, sest komakohti ei näidata.
+// kui kasutame jagamismärgi asemel valemis % märki, näitab ta jääki, ehk mis peale 3x 8 mahtumist 35 sisse veel üle jäi. Vastuseks siis 1.
+
+Console.WriteLine(c);
+
